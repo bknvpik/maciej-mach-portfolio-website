9fb36277bb330139bfda7a80783c51a682288781
Bartosz <b.knvpik@gmail.com>
2022-09-20 00:16:29 +0200

    Add boilerplate for JS components, style a nav menu

---
+++ src/scripts/helpers/compile.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * @param { string } template 
+ * @returns { Element | null }
+ */
+export function compile(template) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(template, 'text/html');
+  return doc.body.firstElementChild;
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,15 @@
 function main() {
-    
+
+  const $body = document.body;
+  const $navToggler = document.getElementById('menu-icon')
+  const $header = document.getElementsByTagName('header')[0];
+  const $navMenu = document.getElementsByClassName('nav-menu')[0];
+
+  $navToggler.addEventListener('click', (event) => {
+    $header.classList.toggle('header-bg-on');
+    $navMenu.classList.toggle('open-menu');
+    $body.classList.toggle('no-scroll');
+  })
 }
 
 window.addEventListener('DOMContentLoaded', main);
