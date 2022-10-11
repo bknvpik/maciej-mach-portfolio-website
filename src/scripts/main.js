91825cade107bfd6ce7bea1800cd32c999251054
Bartosz <b.knvpik@gmail.com>
2022-10-11 18:17:22 +0200

    Improve menu and navbar behaviour, fix bugs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,13 +5,23 @@ function main() {
   const $header = document.getElementsByTagName('header')[0];
   const $navMenu = document.getElementsByClassName('nav-menu')[0];
 
+  const $mainContainer = document.getElementById('main-container');
+  const $landingPage = document.getElementById('landing-page');
+
+  const $menuNavLinks = document.getElementsByClassName('nav-link');
+
   $navToggler.addEventListener('click', (event) => {
-    $header.classList.toggle('header-bg-on');
+    if($mainContainer.scrollTop < $landingPage.getBoundingClientRect().bottom)
+      $header.classList.toggle('header-bg-on');
     $navMenu.classList.toggle('open-menu');
     $body.classList.toggle('no-scroll');
   });
 
-  const $menuNavLinks = document.getElementsByClassName('nav-link');
+  $mainContainer.onscroll = () => {
+    $mainContainer.scrollTop > $landingPage.getBoundingClientRect().bottom 
+    ? $header.classList.add('header-bg-on')
+    : $header.classList.remove('header-bg-on');
+  }
 
   for(let i = 0; i < $menuNavLinks.length; i++) {
     $menuNavLinks[i].addEventListener('click', (event) => {
