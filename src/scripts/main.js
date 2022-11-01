66e32ca84240324d48873d93282835e0da044fc3
Bartosz <b.knvpik@gmail.com>
2022-11-02 00:42:19 +0100

    Add media queries styles, experimental contact form handling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,32 +1,39 @@
 function main() {
 
   const $body = document.body;
-  const $navToggler = document.getElementById('menu-icon')
+  const $navToggler = document.getElementsByClassName('menu-icon')[0];
   const $header = document.getElementsByTagName('header')[0];
   const $navMenu = document.getElementsByClassName('nav-menu')[0];
+  const $navIconLines = document.getElementsByClassName('menu-icon-line');
 
   const $mainContainer = document.getElementById('main-container');
   const $landingPage = document.getElementById('landing-page');
 
-  const $menuNavLinks = document.getElementsByClassName('nav-link');
+  const $menuNavLinks = document.getElementsByClassName('nav-menu__link');
 
   $navToggler.addEventListener('click', (event) => {
     if($mainContainer.scrollTop < $landingPage.getBoundingClientRect().bottom)
-      $header.classList.toggle('header-bg-on');
-    $navMenu.classList.toggle('open-menu');
+      $header.classList.toggle('bg-dark');
+    $navMenu.classList.toggle('nav-menu--open');
     $body.classList.toggle('no-scroll');
+    $navIconLines[1].classList.toggle('hide');
+    $navIconLines[0].classList.toggle('rotate45-right');
+    $navIconLines[2].classList.toggle('rotate45-left');
   });
 
-  $mainContainer.onscroll = () => {
-    $mainContainer.scrollTop > $landingPage.getBoundingClientRect().bottom 
-    ? $header.classList.add('header-bg-on')
-    : $header.classList.remove('header-bg-on');
-  }
+  const mobileMediaQuery = window.matchMedia('(max-width: 80em)');
+
+  if(mobileMediaQuery.matches)
+    $mainContainer.onscroll = () => {
+      $mainContainer.scrollTop > $landingPage.getBoundingClientRect().bottom 
+      ? $header.classList.add('bg-dark')
+      : $header.classList.remove('bg-dark');
+    }
 
   for(let i = 0; i < $menuNavLinks.length; i++) {
     $menuNavLinks[i].addEventListener('click', (event) => {
-      $header.classList.toggle('header-bg-on');
-      $navMenu.classList.toggle('open-menu');
+      $header.classList.toggle('bg-dark');
+      $navMenu.classList.toggle('nav-menu--open');
       $body.classList.toggle('no-scroll');
     });
   };
